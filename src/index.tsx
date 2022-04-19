4d3bf24049f013ef7270bdf4ccd562ae3bb196f6
jpnykw <jpnykw@gmail.com>
2022-04-19 21:41:23 +0900

    :recycle: 相対パスを修正

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
 import Header from './components/header/header'
+import About from './pages/about/about'
 
 const App = () => (
     <>
         <Header />
+        <About />
     </>
 )
 
