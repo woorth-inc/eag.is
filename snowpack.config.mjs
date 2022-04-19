279837c09bf150fd4ab3adee3446cc070e4b21cf
jpnykw <jpnykw@gmail.com>
2022-04-19 23:07:20 +0900

    :package: webpackプラグインを追加  :recycle: buildとdeployコマンドを修正

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -3,8 +3,12 @@ export default {
     public: { url: '/', static: true },
     src: '/dist',
   },
+  optimize: {
+    minify: true,
+  },
   plugins: [
     '@snowpack/plugin-typescript',
     '@snowpack/plugin-react-refresh',
+    '@snowpack/plugin-webpack',
   ],
 }
