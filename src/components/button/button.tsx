5f5de6187034cf9c9ede0a60176b9fa9fab077fb
jpnykw <jpnykw@gmail.com>
2022-04-21 03:17:51 +0900

    :sparkles: buttonコンポーネントを実装

---
+++ src/components/button/button.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import styled from 'styled-components'
+
+interface Props {
+    action: (_: any) => any
+    className: string
+    children: string
+}
+
+const ButtonWrapper = styled.div`
+    border: solid 2px white;
+    border-radius: 6px;
+
+    background: transparent;
+    color: white;
+
+    text-align: center;
+    line-height: 20px;
+    height: 20px;
+    padding: 10px;
+
+    display: block;
+    width: 100px;
+    cursor: pointer;
+`
+
+export default ({
+    action,
+    className,
+    children,
+}: Props) => (
+    <ButtonWrapper className={className} onClick={action}>
+        {children}
+    </ButtonWrapper>
+)
