582a68e44186cb8f0a0777da05afb041d1e1a723
jpnykw <jpnykw@gmail.com>
2022-04-21 03:20:27 +0900

    :art: ページ全体の調節 :sparkles: ドキュメントへのリンクを追加

--- src/pages/about/about.tsx
+++ src/pages/about/about.tsx
@@ -1,37 +1,38 @@
 import React from 'react'
 import styled from 'styled-components'
 import Card from '../../components/card/card'
+import Button from '../../components/button/button'
 import type { State } from '../../components/card/card'
 import './style.css'
 
 const CardWrapper = styled.div`
+    height: 198px;
     position: relative;
-    margin: 0 auto;
-    width: 500px;
+    bottom: 50px;
 `
 
 const StyledCard = styled(Card)`
-    display: inline-block;
-
     position: absolute;
-    left: 50%;
+    transform: translateX(-50%);
+    left: calc(50% + ${({ id }) => ['-300px', '0px', '300px'][id]});
+    z-index: ${({ id }) => 3 - id};
 
-    z-index: ${props => 3 - props.id};
+    opacity: 0;
+    animation: fadein-card 800ms ease ${({ id }) => 900 + id * 120}ms forwards;
 `
 
 const TitleWrapper = styled.div`
-    width: 1200px;
-    margin: 0 auto;
-
     font-weight: 900;
     font-size: 30px;
     text-align: center;
+    transform: scale(1.4, 0.7);
 
+    margin: 0 auto;
     position: relative;
-    top: 100px;
+    top: 30px;
 
     p {
-        font-size: 150px;
+        font-size: 130px;
         font-style: italic;
         margin: -40px 0;
     }
@@ -40,17 +41,17 @@ const TitleWrapper = styled.div`
         {
             id: 1,
             style: `
-                height: 100px;
-                margin-top: 55px;
-                background: linear-gradient(black 0, rgba(255,255,255,0) 100px);
+                height: 320px;
+                margin-top: 40px;
+                background: linear-gradient(black 0, rgba(255,255,255,0) 100%);
             `,
         },
         {
             id: 3,
             style: `
-                height: 200px;
-                margin-top: 60px;
-                background: linear-gradient(rgba(255,255,255,0) 0, black 100px);
+                height: 320px;
+                margin-top: -140px;
+                background: linear-gradient(rgba(255,255,255,0) 0, black 100%);
             `,
         },
     ].map(({ id, style }) => {
@@ -69,34 +70,46 @@ const TitleWrapper = styled.div`
     }).join('\n')}
 `
 
-export default () => (
-    <>
-        <TitleWrapper>
-            {['Easy to get', 'Aegis with', 'Eagis'].map((text, key) => (
-                <p
-                    key={key}
-                    style={{
-                        opacity: '0',
-                        animation: `fadein 800ms ease ${900 + key * 120}ms forwards`,
-                    }}
-                >
-                    {text}
-                </p>
-            ))}
-        </TitleWrapper>
+const StyledButton = styled(Button)`
+    margin: 40px auto;
+`
 
-        <CardWrapper>
-            {['safe', 'unsafe', 'unknown'].map((state: State, key) => (
-                <StyledCard
-                    type={'NFT'}
-                    name={'VeryLongAnimals'}
-                    icon={'https://lh3.googleusercontent.com/fIn8q46M8M9RnmMOz3Kf1sAKlJRUw3nFAt6xX25Bxtw62BAhrN0CyVTKZz8BQ3O4-C6sLaD2vlTU_sYHL7737-YaK2uKZwjvuikQhg=s0'}
-                    state={state}
-                    detail={'Super Long Animal'}
-                    id={key}
-                    key={key}
-                />
-            ))}
-        </CardWrapper>
-    </>
-)
+export default () => {
+    const handleButtonClick = () => {
+        window.open('https://docs.eag.is/');
+    }
+
+    return (
+        <>
+            <TitleWrapper>
+                {['Easy to get', 'Aegis with', 'Eagis'].map((text: string, key: number) => (
+                    <p
+                        key={key}
+                        style={{
+                            opacity: '0',
+                            animation: `fadein-title 800ms ease ${900 + key * 120}ms forwards`,
+                        }}
+                    >
+                        {text}
+                    </p>
+                ))}
+            </TitleWrapper>
+
+            <CardWrapper>
+                {['safe', 'unsafe', 'unknown'].map((state: State, key: number) => (
+                    <StyledCard
+                        type={'NFT'}
+                        name={'Collection Name'}
+                        icon={'https://lh3.googleusercontent.com/fIn8q46M8M9RnmMOz3Kf1sAKlJRUw3nFAt6xX25Bxtw62BAhrN0CyVTKZz8BQ3O4-C6sLaD2vlTU_sYHL7737-YaK2uKZwjvuikQhg=s0'}
+                        state={state}
+                        detail={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '}
+                        id={key}
+                        key={key}
+                    />
+                ))}
+            </CardWrapper>
+
+            <StyledButton action={handleButtonClick}>Docs</StyledButton>
+        </>
+    )
+}
