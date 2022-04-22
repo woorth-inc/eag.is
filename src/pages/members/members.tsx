da5aed65b9920b82f2e0104397f622aa335c2a2d
jpnykw <jpnykw@gmail.com>
2022-04-22 12:08:33 +0900

    :art: membersページにコンテンツを配置した

--- src/pages/members/members.tsx
+++ src/pages/members/members.tsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import styled from 'styled-components'
 import Member from '../../components/member/member'
+
+import Taisuke from '../../assets/images/members-icon/nakano_taisuke.jpg'
+import Haruto from '../../assets/images/members-icon/hirakawa_haruto.jpg'
+import Reiya from '../../assets/images/members-icon/oishi_reiya.jpg'
+import Kazuya from '../../assets/images/members-icon/okumura_kazuya.jpg'
+
 import './style.css'
 
 interface Props {
@@ -8,8 +14,8 @@ interface Props {
     firstRender: boolean
 }
 
-const DummyContainer = styled.div`
-    font-size: 24px;
+const MembersContainer = styled.div`
+    font-size: 22px;
     text-align: center;
 
     position: absolute;
@@ -18,8 +24,8 @@ const DummyContainer = styled.div`
     top: 50%;
 
     display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    grid-gap: 170px;
+    grid-template-columns: repeat(${({ members }) => members}, 1fr);
+    grid-gap: 60px;
 
     opacity: ${({ animation }) => animation === 'fadein' ? 0 : animation === 'fadeout' ? 1 : 0};
     ${({ animation, firstRender }) => firstRender ? '' : `animation: ${animation}-member 800ms ease 50ms forwards;`}
@@ -28,15 +34,46 @@ const DummyContainer = styled.div`
 export default ({
     animation,
     firstRender,
-}: Props) => (
-    <DummyContainer
-        animation={animation}
-        firstRender={firstRender}
-    >
-        {['1', '2', '3'].map((_) => {
-            return (
-                <Member />
-            )
-        })}
-    </DummyContainer>
-)
+}: Props) => {
+    const members = [
+        {
+            name: 'Taisuke Nakano',
+            icon: Taisuke,
+            twitter: 'ta1suke',
+            github: 'ta1suke',
+        },
+        {
+            name: 'Haruto Hirakawa',
+            icon: Haruto,
+            twitter: 'jpnykw',
+            github: 'jpnykw',
+        },
+        {
+            name: 'Reiya Oishi',
+            icon: Reiya,
+            twitter: 'mi0256',
+            github: 'mio256',
+        },
+        {
+            name: 'Okumura Kazuya',
+            icon: Kazuya,
+            twitter: 'mixaz017',
+            github: 'mixaz017',
+        }
+    ]
+
+    return (
+        <MembersContainer
+            animation={animation}
+            firstRender={firstRender}
+            members={members.length}
+        >
+            {members.map((params, id) => (
+                <Member
+                    {...params}
+                    key={id}
+                />
+            ))}
+        </MembersContainer>
+    )
+}
