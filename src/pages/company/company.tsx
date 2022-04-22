624f599fa8c6d41799e685a3bd38d3910d02953a
jpnykw <jpnykw@gmail.com>
2022-04-22 10:18:31 +0900

    :recycle: ページの差し替えとcssの修正

---
+++ src/pages/company/company.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import styled from 'styled-components'
+import './style.css'
+
+interface Props {
+    animation: string
+    firstRender: boolean
+}
+
+const DummyContainer = styled.div`
+    font-size: 24px;
+    position: absolute;
+    transform: translate(-50%, -50%);
+    left: 50%;
+    top: 50%;
+
+    opacity: ${({ animation }) => animation === 'fadein' ? 0 : animation === 'fadeout' ? 1 : 0};
+    ${({ animation, firstRender }) => firstRender ? '' : `animation: ${animation}-company 800ms ease 50ms forwards;`}
+`
+
+export default ({
+    animation,
+    firstRender,
+}: Props) => (
+    <DummyContainer
+        animation={animation}
+        firstRender={firstRender}
+    >
+        <p>company</p>
+        <p>(=^・・^=)</p>
+    </DummyContainer>
+)
---
+++ src/pages/member/member.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import styled from 'styled-components'
+import './style.css'
+
+interface Props {
+    animation: string
+    firstRender: boolean
+}
+
+const DummyContainer = styled.div`
+    font-size: 24px;
+    text-align: center;
+
+    position: absolute;
+    transform: translate(-50%, -50%);
+    left: 50%;
+    top: 50%;
+
+    opacity: ${({ animation }) => animation === 'fadein' ? 0 : animation === 'fadeout' ? 1 : 0};
+    ${({ animation, firstRender }) => firstRender ? '' : `animation: ${animation}-member 800ms ease 50ms forwards;`}
+`
+
+export default ({
+    animation,
+    firstRender,
+}: Props) => (
+    <DummyContainer
+        animation={animation}
+        firstRender={firstRender}
+    >
+        <p>members</p>
+        <p>・(=^・・^=)</p>
+    </DummyContainer>
+)
