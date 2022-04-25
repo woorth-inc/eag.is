cf32680eb0d5af871f5d43d149e20dc41501f76f
jpnykw <jpnykw@gmail.com>
2022-04-26 07:02:28 +0900

    :sparkles: memberesページをレスポンシブ対応

--- src/pages/members/members.tsx
+++ src/pages/members/members.tsx
@@ -9,22 +9,28 @@ import Kazuya from '../../assets/images/members-icon/okumura_kazuya.jpg'
 
 import './style.css'
 
+interface Media {
+    isMobile: boolean
+    isTablet: boolean
+}
+
 interface Props {
     animation: string
     firstRender: boolean
-    isMobile: boolean
+    media: Media
 }
 
-const MembersContainer = styled.div`
+const Container = styled.div`
+    pointer-events: ${({ disableClick }) => disableClick ? 'none' : 'all'};
     font-size: 22px;
     text-align: center;
+    display: block;
 
-    ${({ isMobile }) => {
-        return isMobile ? `
-            position: absolute;
-            transform: translateX(-50%);
-            left: 50%;
-            margin-top: 300px;
+    ${({ media }) => {
+        return media.isTablet ? `
+            margin-top: 20px;
+            width: 300px;
+            margin: 100px auto;
         ` : `
             position: absolute;
             transform: translate(-50%, -50%);
@@ -33,24 +39,26 @@ const MembersContainer = styled.div`
         `
     }}
 
+    opacity: ${({ animation, media }) => media.isTablet ? 1 : animation === 'fadein' ? 0 : animation === 'fadeout' ? 1 : 0};
+    ${({ animation, firstRender, media }) => media.isTablet || firstRender ? '' : `animation: ${animation}-member 800ms ease 50ms forwards;`}
+`
+
+const MemberWrapper = styled.div`
     display: grid;
-    grid-gap: 40px;
-    ${({ members, isMobile }) => {
-        return isMobile ? `
+    grid-gap: 20px;
+    ${({ members, media }) => {
+        return media.isTablet ? `
             grid-template-rows: repeat(${members}, 1fr);
         ` : `
             grid-template-columns: repeat(${members}, 1fr);
         `
     }}
-
-    opacity: ${({ animation, isMobile }) => isMobile ? 1 : animation === 'fadein' ? 0 : animation === 'fadeout' ? 1 : 0};
-    ${({ animation, firstRender, isMobile }) => isMobile || firstRender ? '' : `animation: ${animation}-member 800ms ease 50ms forwards;`}
 `
 
 export default ({
     animation,
     firstRender,
-    isMobile,
+    media,
 }: Props) => {
     const members = [
         {
@@ -80,20 +88,38 @@ export default ({
     ]
 
     return (
-        <MembersContainer
+        <Container
             animation={animation}
             firstRender={firstRender}
-            isMobile={isMobile}
-            members={members.length}
+            disableClick={animation !== 'fadein' && !media.isTablet}
+            media={media}
         >
-            {members.map((params, id) => (
-                <Member
-                    {...params}
-                    animation={animation}
-                    firstRender={firstRender}
-                    key={id}
-                />
-            ))}
-        </MembersContainer>
+            {
+                media.isTablet && (
+                    <div style={{
+                        marginBottom: '20px',
+                        fontFamily: '"Noto Sans JP"',
+                        fontWeight: '500',
+                        fontSize: media.isMobile ? '18px' : '23px',
+                    }}>
+                        👥メンバー
+                    </div>
+                )
+            }
+            <MemberWrapper
+                media={media}
+                members={members.length}
+            >
+                {members.map((params, id) => (
+                    <Member
+                        {...params}
+                        animation={animation}
+                        firstRender={firstRender}
+                        media={media}
+                        key={id}
+                    />
+                ))}
+            </MemberWrapper>
+        </Container>
     )
 }
