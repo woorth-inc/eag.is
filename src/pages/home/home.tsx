6674e510f58102c908a341aa6b0881b76730ce9d
jpnykw <jpnykw@gmail.com>
2022-04-26 07:02:14 +0900

    :sparkles: homeページをレスポンシブ対応

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -7,22 +7,37 @@ import type { State } from '../../components/card/card'
 import PopupSafe from '../../assets/images/popup/safe.png'
 import PopupUnsafe from '../../assets/images/popup/unsafe.png'
 import PopupUnknown from '../../assets/images/popup/unknown.png'
+import PopupAllInOne from '../../assets/images/popup/all_in_one.png'
 
 import './style.css'
+
+interface Media {
+    isMobile: boolean
+    isTablet: boolean
+}
 
 interface Props {
     animation: string
     firstRender: boolean
-    isMobile: boolean
+    media: Media
 }
 
 const Container = styled.div`
-    ${({ isMobile }) => {
-        return isMobile ? `
+    pointer-events: ${({ disableClick }) => disableClick ? 'none' : 'all'};
+    display: block;
+
+    ${({ media }) => {
+        return media.isTablet ? `
             position: relative;
             bottom: 50px;
         ` : ''
     }}
+`
+
+const CardWrapper = styled.div`
+    position: relative;
+    height: ${({ media }) => media.isMobile ? '162px' : media.isTablet ? '135px' : '198px'};
+    bottom: ${({ media }) => media.isTablet ? '21px' : '59px'};
 `
 
 const StyledCard = styled(Card)`
@@ -38,10 +53,28 @@ const StyledCard = styled(Card)`
     animation: ${({ animation }) => `${animation}-card`} 800ms ease ${({ id, firstRender }) => (firstRender ? 900 : 50) + id * 120}ms forwards;
 `
 
-const CardWrapper = styled.div`
-    height: 198px;
+const CardForTablet = styled.img`
+    position: absolute;
+    width: 250px;
+
+    transform: translateX(-50%);
+    left: calc(50% + ${({ id }) => [-170, 0, 170][id]}px);
+    z-index: calc(3 - ${({ id }) => id});
+
+    -webkit-box-shadow: 0px 0px 15px 7px rgba(0, 0, 0, 0.3);
+    box-shadow: 0px 0px 15px 7px rgba(0, 0, 0, 0.3);
+
+    opacity: ${({ animation }) => animation === 'fadein' ? 0 : 1};
+    animation: ${({ animation }) => `${animation}-card`} 800ms ease ${({ id, firstRender }) => (firstRender ? 900 : 50) + id * 120}ms forwards;
+`
+
+const CardForMobile = styled.img`
     position: relative;
-    bottom: ${({ isMobile }) => isMobile ? '20px' : '50px'};
+    transform: translateX(-50%);
+    left: 50%;
+    width: 400px;
+    top: 70px;
+
 `
 
 const Title = styled.p`
@@ -53,20 +86,20 @@ const TitleWrapper = styled.div`
     font-weight: 900;
     font-size: 30px;
     text-align: center;
-    transform: scale(1.4, 0.7);
+    ${({ media }) => media.isMobile ? '' : 'transform: scale(1.4, 0.7)'};
 
     margin: 0 auto;
     position: relative;
-    top: 30px;
+    top: ${({ media }) => media.isMobile ? '60px' : '30px'};
 
     p {
         font-style: italic;
-        font-size: ${({ isMobile }) => isMobile ? '60px' : '130px'};
-        margin: ${({ isMobile }) => isMobile ? '-10px' : '-40px'} 0;
+        font-size: ${({ media }) => media.isMobile ? '35px' : media.isTablet ? '60px' : '130px'};
+        margin: ${({ media }) => media.isTablet ? '-10px' : '-40px'} 0;
     }
 
-    ${({ isMobile }) => {
-        return isMobile ? `
+    ${({ media }) => {
+        return media.isTablet ? `
         ` : [
             {
                 id: 1,
@@ -102,28 +135,52 @@ const TitleWrapper = styled.div`
 `
 
 const StyledButton = styled(Button)`
-    margin: 40px auto;
     width: 110px;
+    margin: ${({ media }) => media.isMobile ? '20px' : '40px'} auto;
     opacity: ${({ animation }) => animation === 'fadein' ? 0 : 1};
     animation: ${({ animation }) => `${animation}-button`} 800ms ease ${({ firstRender }) => firstRender ? 600 : 50}ms forwards;
+`
+
+const Description = styled.div`
+    margin: 0 auto;
+    width: 80%;
+    text-align: center;
+    margin-top: ${({ media }) => media.isTablet ? '20px' : '0'};
+
+    & > p {
+        margin: 0;
+    }
+
+    & > p:nth-of-type(1) {
+        opacity: ${({ animation }) => animation === 'fadein' ? 0 : 1};
+        animation: ${({ animation }) => `${animation}-description-line1`} 800ms ease ${({ firstRender }) => firstRender ? 600 : 50}ms forwards;
+    }
+
+    & > p:nth-of-type(2) {
+        opacity: ${({ animation }) => animation === 'fadein' ? 0 : 1};
+        animation: ${({ animation }) => `${animation}-description-line2`} 800ms ease ${({ firstRender }) => firstRender ? 600 : 50}ms forwards;
+    }
 `
 
 export default ({
     animation,
     firstRender,
-    isMobile,
+    media,
 }: Props) => {
     const handleDocsClick = () => {
         open('https://docs.eag.is/')
     }
 
     const handlePreRegisterClick = () => {
-        open('blank') // TODO: リンクを追加
+        open('https://notionforms.io/forms/eagis-pre-register-form')
     }
 
     return (
-        <Container isMobile={isMobile}>
-            <TitleWrapper isMobile={isMobile}>
+        <Container
+            media={media}
+            disableClick={animation !== 'fadein'}
+        >
+            <TitleWrapper media={media}>
                 {['Easy to get', 'Aegis with', 'Eagis'].map((text: string, id: number) => (
                     <Title
                         animation={animation}
@@ -136,24 +193,25 @@ export default ({
                 ))}
             </TitleWrapper>
 
-            {/* TODO: モバイルの場合は画像を表示する */}
-            <CardWrapper isMobile={isMobile}>
+            <CardWrapper media={media}>
                 {
-                    isMobile ? (
-                        [PopupSafe, PopupUnsafe, PopupUnknown].map((src, id: number) => (
-                            <img
-                                style={{
-                                    position: 'absolute',
-                                    left: `calc(50% + ${[-300, 0, 300][id]}px)`,
-                                    transform: 'translateX(-50%)',
-                                    zIndex: 3 - id,
-                                    WebkitBoxShadow: '0px 0px 15px 7px rgba(0, 0, 0, 0.3)',
-                                    boxShadow: '0px 0px 15px 7px rgba(0, 0, 0, 0.3)',
-                                }}
-                                src={src}
-                                key={id}
-                            />
-                        ))
+                    media.isMobile ? (
+                        <CardForMobile
+                            src={PopupAllInOne}
+                            alt={'popup example'}
+                        />
+                    ) : media.isTablet ? (
+                        [PopupSafe, PopupUnsafe, PopupUnknown].map((src, id: number) => {
+                            return (
+                                <CardForTablet
+                                    animation={animation}
+                                    firstRender={firstRender}
+                                    src={src}
+                                    id={id}
+                                    key={id}
+                                />
+                            )
+                        })
                     ) : (
                         ['safe', 'unsafe', 'unknown'].map((state: State, id: number) => (
                             <StyledCard
@@ -172,10 +230,24 @@ export default ({
                 }
             </CardWrapper>
 
+            {
+                media.isTablet && (
+                    <Description
+                        animation={animation}
+                        firstRender={firstRender}
+                        media={media}
+                    >
+                        <p>NFT真贋鑑定や危険なサイトの判定を行う</p>
+                        <p>Web3時代の無料セキュリティツール</p>
+                    </Description>
+                )
+            }
+
             <div
                 style={
-                    isMobile ? {
-                        margin: '20px 0',
+                    media.isMobile ? {
+                        marginTop: '50px',
+                        marginBottom: '20px',
                     } : {
                         display: 'flex',
                         width: '300px',
@@ -187,6 +259,7 @@ export default ({
                     action={handleDocsClick}
                     animation={animation}
                     firstRender={firstRender}
+                    media={media}
                 >
                     Docs
                 </StyledButton>
@@ -194,6 +267,7 @@ export default ({
                     action={handlePreRegisterClick}
                     animation={animation}
                     firstRender={firstRender}
+                    media={media}
                 >
                     Pre-Register
                 </StyledButton>
