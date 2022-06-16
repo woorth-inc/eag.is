16b91570f845592f51e4fc1532ab728585d39d30
jpnykw <jpnykw@gmail.com>
2022-06-16 19:23:55 +0900

    Updates

--- dist/pages/partners/style.css.proxy.js
+++ dist/pages/partners/style.css.proxy.js
@@ -1,19 +1,19 @@
-if(typeof document!="undefined"){const e=`@keyframes fadein-company {
-    from {
-        opacity: 0;
-    }
-    to {
-        opacity: 1;
-        pointer-events: all;
-    }
-}
-
-@keyframes fadeout-company {
-    from {
-        opacity: 1;
-    }
-    to {
-        opacity: 0;
-        pointer-events: none;
-    }
+if(typeof document!="undefined"){const e=`@keyframes fadein-company {\r
+    from {\r
+        opacity: 0;\r
+    }\r
+    to {\r
+        opacity: 1;\r
+        pointer-events: all;\r
+    }\r
+}\r
+\r
+@keyframes fadeout-company {\r
+    from {\r
+        opacity: 1;\r
+    }\r
+    to {\r
+        opacity: 0;\r
+        pointer-events: none;\r
+    }\r
 }`,n=document.createElement("style"),t=document.createTextNode(e);n.type="text/css",n.appendChild(t),document.head.appendChild(n)}
--- dist/style.css.proxy.js
+++ dist/style.css.proxy.js
@@ -1,49 +1,49 @@
-if(typeof document!="undefined"){const t=`@keyframes disappear-about {
-    from {
-        opacity: 1;
-        transform: rotateX(0);
-    }
-    to {
-        opacity: 0;
-        transform: rotateX(90deg);
-        pointer-events: none;
-    }
-}
-
-@keyframes appear-about {
-    from {
-        opacity: 0;
-        transform: rotateX(-90deg);
-    }
-    to {
-        opacity: 1;
-        transform: rotateX(0deg);
-        pointer-events: all;
-    }
-}
-
-/* dummy */
-
-@keyframes disappear-dummy {
-    from {
-        opacity: 1;
-        transform: rotateX(0) translate(-50%, -50%);
-    }
-    to {
-        opacity: 0;
-        transform: rotateX(90deg) translate(-50%, -50%);
-        pointer-events: none;
-    }
-}
-
-@keyframes appear-dummy {
-    from {
-        opacity: 0;
-        transform: rotateX(-90deg) translate(-50%, -50%);
-    }
-    to {
-        opacity: 1;
-        transform: rotateX(0deg) translate(-50%, -50%);
-        pointer-events: all;
-    }
-}`,n=document.createElement("style"),e=document.createTextNode(t);n.type="text/css",n.appendChild(e),document.head.appendChild(n)}
+if(typeof document!="undefined"){const n=`@keyframes disappear-about {\r
+    from {\r
+        opacity: 1;\r
+        transform: rotateX(0);\r
+    }\r
+    to {\r
+        opacity: 0;\r
+        transform: rotateX(90deg);\r
+        pointer-events: none;\r
+    }\r
+}\r
+\r
+@keyframes appear-about {\r
+    from {\r
+        opacity: 0;\r
+        transform: rotateX(-90deg);\r
+    }\r
+    to {\r
+        opacity: 1;\r
+        transform: rotateX(0deg);\r
+        pointer-events: all;\r
+    }\r
+}\r
+\r
+/* dummy */\r
+\r
+@keyframes disappear-dummy {\r
+    from {\r
+        opacity: 1;\r
+        transform: rotateX(0) translate(-50%, -50%);\r
+    }\r
+    to {\r
+        opacity: 0;\r
+        transform: rotateX(90deg) translate(-50%, -50%);\r
+        pointer-events: none;\r
+    }\r
+}\r
+\r
+@keyframes appear-dummy {\r
+    from {\r
+        opacity: 0;\r
+        transform: rotateX(-90deg) translate(-50%, -50%);\r
+    }\r
+    to {\r
+        opacity: 1;\r
+        transform: rotateX(0deg) translate(-50%, -50%);\r
+        pointer-events: all;\r
+    }\r
+}`,r=document.createElement("style"),t=document.createTextNode(n);r.type="text/css",r.appendChild(t),document.head.appendChild(r)}
